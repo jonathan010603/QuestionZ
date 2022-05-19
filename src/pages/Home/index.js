d84162f6ac40162d6fd0c0db63322f97bb3b044a
jonathan010603 <jonathan010603@gmail.com>
2022-05-19 01:26:44 -0300

    fixing deployment

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,9 @@ import "./home.css";
 export default function Home({ nextPage }) {
   return (
     <div id={"homeContainer"}>
+      <a href="https://github.com/jonathan010603/questionz" id={"codeLink"}>
+        <img src="/codeIcon.png" id={"codeLinkImage"}/>
+      </a>
       <div id={"dialogue"}>
         <span>Welcome to Questionz!</span>
         <span>Are you looking for a challenge?</span>
--- src/pages/navbar/index.js
+++ src/pages/navbar/index.js
@@ -4,7 +4,6 @@ import "./navbar.css"
 export default function Navbar() {
   return (
     <nav id={"nav"}>
-      <a href=""><img src="/codeIcon.png" id={"codeLink"}/></a>
       <span id={"title"}>
         <a href="/">QuestionZ</a>
       </span>
