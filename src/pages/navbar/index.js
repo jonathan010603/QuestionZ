9a70d1be8b4f9cca14ebdd0d2ed15cdd7f580b0f
jonathan010603 <jonathan010603@gmail.com>
2022-05-19 02:23:13 -0300

    some linking fixings

--- src/pages/navbar/index.js
+++ src/pages/navbar/index.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <nav id={"nav"}>
       <span id={"title"}>
-        <a href="/">QuestionZ</a>
+        <a href="https://jonathan010603.github.io/questionz">QuestionZ</a>
       </span>
     </nav>
   );
