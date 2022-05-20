2e8b4ee9c6dc3cfd534029924da31de742c5ee29
Jonathan da Silva <76676185+jonathan010603@users.noreply.github.com>
2022-05-20 10:04:19 -0300

    Adding LogRocket Adding LogRocket to track the errors and monitoring the app

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Pages from "./pages";
+import LogRocket from "logrocket";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+LogRocket.init("cq2qud/questionz");
 
 root.render(
   <StrictMode>
